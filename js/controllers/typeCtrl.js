95e756dd5ab01d2f93a2010a571373e001412f93
Jared Hensley <jahensle85@me.com>
2015-11-15 11:34:59 -0600

    typeCtrl postTodo function

--- js/controllers/typeCtrl.js
+++ js/controllers/typeCtrl.js
@@ -1,17 +1,33 @@
-angular.module('todo').controller('typeCtrl', function($scope, todos, $stateParams){
+angular.module('todo').controller('typeCtrl', function ($scope, todos, $stateParams, parseService) {
   switch ($stateParams.status) {
-    case 'new':
-      $scope.todos = todos.newTodos
-      $scope.status = 'New'
-      break;
-    case 'inProg':
-      $scope.todos = todos.inProgTodos
-      $scope.status = 'In Progress'
-      break;
-    case 'done':
-      $scope.todos = todos.doneTodos
-      $scope.status = 'Done'
-      break;
+  case 'new':
+    $scope.todos = todos.newTodos
+    $scope.status = 'New'
+    break;
+  case 'inProg':
+    $scope.todos = todos.inProgTodos
+    $scope.status = 'In Progress'
+    break;
+  case 'done':
+    $scope.todos = todos.doneTodos
+    $scope.status = 'Done'
+    break;
   }
+
+  $scope.postTodo = function (todo) {
+    todoObj = {
+      title: $scope.newTodo,
+      status: 'new'
+    }
+    parseService.postTodo(todo)
+      .then(function (res) {
+        if (res.status === 201) {
+          $scope.getTodos()
+          $scope.newTodo = ''
+        }
+      })
+  }
+
+
   console.log('todos for ' + $stateParams.status + ':', $scope.todos)
 })
